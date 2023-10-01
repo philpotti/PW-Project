331fb6a81f0cc33e2c37958e13421bc71b2da552
MacFusion92 <i.haslop@fusion92.com>
2023-10-01 11:00:16 -0300

    Adding E2E Test plus API E2E

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,31 @@
+const {test, expect} = require('@playwright/test');
+
+test('Popup validations', async({page}) =>{
+    
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+    // await page.goto('http://google.com');
+    // await page.goBack();
+    // await page.goForward();    
+
+    // ASSERT ON OBJECT WHEN IS {{VISIBLE}} OR {{HIDDEN}}
+    await expect (page.locator('#displayed-text')).toBeVisible();
+    await page.locator('#hide-textbox').click();
+    await expect (page.locator('#displayed-text')).toBeHidden();
+
+    // // await page.pause();
+
+    // POP-UP: Handling Pop-ups
+        // ACCEPT OR DISMISS a POP-UP
+        // page.on('dialog', dialog => dialog.dismiss());
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator('#confirmbtn').click();
+
+    // HOW TO HOVER OVER ELEMENTS
+    await page.locator('#mousehover').hover();
+
+    // iFrame HANDLING    
+    const framesPage = page.frameLocator('#courses-iframe');
+    await framesPage.locator('li a[href*="lifetime-access"]:visible').click(); //li a[href*=lifetime-access] locator has 2 elements (1 visible and 1 invisible. :visible will select the visible)
+    const textCheck = await framesPage.locator('div h2:has-text(" Happy Subscibers!")').textContent();
+    console.log(textCheck.split(" ")[1]);
+});
---
+++ tests/RunningAPITests.spec.js
@@ -0,0 +1,113 @@
+const { test, expect, request } = require('@playwright/test');
+const loginPayLoad = {userEmail: "imhaslop@gmail.com", userPassword: "Passwd321@_"};
+const orderPayLoad = {orders: [{country: "India", productOrderedId: "6262e95ae26b7e1a10e89bf0"}]};
+let token;
+let orderId;
+
+test.beforeAll( async () => 
+{
+    const apiContext = await request.newContext();
+    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+        {                                       
+            data: loginPayLoad
+        }) //200, 201,
+        expect(loginResponse.ok()).toBeTruthy();
+        const loginResponseJson = await loginResponse.json();
+        token = loginResponseJson.token;
+        console.log(token);
+
+        //
+        const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            data: orderPayLoad,
+            headers: 
+            {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        })
+        const orderResponseJson = await orderResponse.json();
+        console.log(orderResponseJson);
+        orderId = orderResponseJson.orders[0]
+});
+
+test.beforeEach( () => {
+
+});
+
+test('Browser context Playwright Test', async ({ page }) => 
+{
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token);                                      
+                                                                // <<<<<<<<<<<<<<FOLLOWING STEPS CAN BE SKIPPED BY USING API>>>>>>>>>>>>>>>>>>>>
+    await page.goto('https:rahulshettyacademy.com/client/');    // await page.goto('https:rahulshettyacademy.com/client')
+    const productName = "zara coat 3";                          // await page.locator('#userEmail').fill('imhaslop@gmail.com');
+    const products = page.locator('.card-body');                // await page.locator('#userPassword').fill('Passwd321@_');
+    // const email = userEmail;                                 // await page.locator('[class*="login-btn"]').click();
+
+    // await page.waitForLoadState('networkidle'); /* This makes the framework to wait till everything else is loaded on the page to carry on with the steps. Avoids innecesary waits */
+    // await page.locator('[class*="card-body"] b').first().waitFor(); /* This is a different way to wait for the element/s to be visible. waitForLoadState() method can be a little flaky */
+    // const titles = await page.locator('[class*="card-body"] b').allTextContents();
+    // console.log(titles);
+    // //
+    // const count = await products.count();
+    
+    // for(let i = 0; i < count; ++i) {
+    //     if(await products.nth(i).locator("b").textContent() === productName) {
+    //         await products.nth(i).locator("text= Add To Cart").click();
+    //         break;
+    //     }
+    // }
+    // // ONCE ADDED THE ITEM TO THE CART, WE CLICK ON THE CART BUTTON TO GO TO A DIFFERENT PAGE
+    // await page.locator("[routerlink*='/cart']").click();
+    // await page.locator("div li").first().waitFor();
+
+    // //ASSERT THAT THE PRODUCT THAT HAS BEEN ADDED TO THE CART IS VISIBLE
+    // const isAddedProductVisible = await page.locator("h3:has-text('zara coat 3')").isVisible();
+    // expect(isAddedProductVisible).toBeTruthy();
+    
+    // await page.locator('text=Checkout').click();
+    
+    // // SELECTING COUNTRY IN A DYNAMIC DROPDOWN. WITH FOR LOOK WE'RE LOOKING FOR INDIA
+    // await page.locator('[placeholder="Select Country"]').pressSequentially('ind',{delay:800})
+    // const countryDropDown = page.locator('[class*="ta-results"]');
+    // await countryDropDown.waitFor();
+    // const optionsCount = await countryDropDown.locator('button').count();
+    // for(let i = 0; i < optionsCount; i++){
+    //     const text = await countryDropDown.locator('button').nth(i).textContent();
+    //     // if(text === ' India')
+    //     if(text.trim() === 'India')
+    //     // if(text.includes('India'))
+    //     {
+    //         await countryDropDown.locator('button').nth(i).click();
+    //         break;
+    //     }
+    // }
+    // // ASSERT ON THE EMAIL AND THEN SUBMIT MY ORDER
+    // // await expect(page.locator('[class*="user__name"] label')).toHaveValue(userEmail);   <<<<<<<< NEED TO RESOLVE >>>>>>>>>>>
+    // await page.locator('.action__submit').click();
+    
+    // // ASSERT THAT WE GET THE ORDER SUCCESSFULLY HAS BEEN SUBMITTED AND GRABBING THE TEXT OF THE orderId
+    // await expect(page.locator('.hero-primary')).toHaveText(' Thankyou for the order. ');
+    // const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    // console.log(orderId);
+
+    // GO TO MY ORDERS PAGE AND VERIFY THAT MY orderId IS VISIBLE IN THE ODERS TABLE
+    await page.locator('button[routerlink*="myorders"]').click();
+    const orderPageRows = page.locator('tbody tr')
+    await orderPageRows.last().waitFor();
+
+    for(let i = 0; i < await orderPageRows.count(); i++){
+        const rowOrderId = await orderPageRows.nth(i).locator('th').textContent();
+        if(orderId.includes(rowOrderId)) {
+            await orderPageRows.nth(i).locator('button:has-text("View")').click();
+            break;
+            }
+        }
+    await page.locator('.email-container').first().waitFor();
+    const orderIdDetailsPage = await page.locator('.col-text').textContent();
+    await page.pause();
+    expect(orderId.includes(orderIdDetailsPage)).toBeTruthy();
+    }
+);
